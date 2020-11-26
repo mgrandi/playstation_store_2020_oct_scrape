470f86f390f36d1e6e150db26888d3d318fe3af9
Mark Grandi <markgrandi@gmail.com>
2020-11-26 04:22:12 -0800

    few changes:
    * make a JsonSearchQuery object that will hold the type of url its for, the
    uncompiled jmespath query and the compiled jmespath query in one object
    * change a few logging statements to be debug
    * add a check for `/image` in chihiro urls , since the image url contains
    the JSON url as a prefix, except with `<some number>/image` added to the end
    so we were attempting to parse a image as JSON which didn't work

--- wpull_plugins/ps_store_json_api_wpull_plugin.py
+++ wpull_plugins/ps_store_json_api_wpull_plugin.py
@@ -7,12 +7,15 @@ from wpull.application.plugin import WpullPlugin, PluginFunctions, hook, event
 from wpull.pipeline.session import ItemSession
 
 import jmespath
+import jmespath.parser
 
 logger = logging.getLogger(__name__)
 
+# logger.setLevel("WARNING")
 
 VALKYRIE_URL_PREFIX = "https://store.playstation.com/valkyrie-api/"
 CHIHIRO_URL_PREFIX = "https://store.playstation.com/store/api/chihiro/"
+CHIHIRO_IMAGE_URL_SUFFIX = "/image"
 
 
 class UrlType(enum.Enum):
@@ -20,14 +23,31 @@ class UrlType(enum.Enum):
     CHIHIRO = 2
 
 
-class PsStoreJsonApiWpullPlugin(WpullPlugin):
+@attr.s(auto_attribs=True, kw_only=True, frozen=True)
+class JsonSearchQuery:
+    url_type:UrlType = attr.ib()
+    jmespath_query:str = attr.ib()
+    jmespath_compiled_query:jmespath.parser.ParsedResult = attr.ib()
 
+
+class PsStoreJsonApiWpullPlugin(WpullPlugin):
 
 
     def __debug_logging_tree(self):
 
         import logging_tree
         print("{}".format( logging_tree.format.build_description(node=None)))
+
+
+    def _get_json_search_query(self, url_type:UrlType, jmespath_query:str):
+        ''' helper to return a JsonSearchQuery object from
+        a UrlType and a jmespath_query
+        '''
+
+        return JsonSearchQuery(
+            url_type=url_type,
+            jmespath_query=jmespath_query,
+            jmespath_compiled_query=jmespath.compile(jmespath_query) )
 
 
 
@@ -38,29 +58,27 @@ class PsStoreJsonApiWpullPlugin(WpullPlugin):
         # use `[]` instead of `[*]` to flatten projections
         # see https://jmespath.org/tutorial.html#flatten-projections
         self.valkyrie_jmespath_expressions = [
-            jmespath.compile('''included[].attributes."content-rating".url'''),
-            jmespath.compile('''included[].attributes."thumbnail-url-base"'''),
-            jmespath.compile('''included[].attributes.parent.thumbnail'''),
-            jmespath.compile('''included[].attributes."media-list".preview.url'''),
-            jmespath.compile('''included[].attributes."media-list".promo.images[].url'''),
-            jmespath.compile('''included[].attributes."media-list".promo.videos[].url'''),
-            jmespath.compile('''included[].attributes."media-list".screenshots[].url'''),
+            self._get_json_search_query(UrlType.VALKYRIE, '''included[].attributes."content-rating".url'''),
+            self._get_json_search_query(UrlType.VALKYRIE, '''included[].attributes."thumbnail-url-base"'''),
+            self._get_json_search_query(UrlType.VALKYRIE, '''included[].attributes.parent.thumbnail'''),
+            self._get_json_search_query(UrlType.VALKYRIE, '''included[].attributes."media-list".preview.url'''),
+            self._get_json_search_query(UrlType.VALKYRIE, '''included[].attributes."media-list".promo.images[].url'''),
+            self._get_json_search_query(UrlType.VALKYRIE, '''included[].attributes."media-list".promo.videos[].url'''),
+            self._get_json_search_query(UrlType.VALKYRIE, '''included[].attributes."media-list".screenshots[].url'''),
             ]
 
         self.chihiro_jmespath_expressions = [
-            jmespath.compile('''content_rating.url'''),
-            jmespath.compile('''images[].url'''),
-            jmespath.compile('''promomedia[].materials[].urls[].url'''),
+            self._get_json_search_query(UrlType.CHIHIRO, '''content_rating.url'''),
+            self._get_json_search_query(UrlType.CHIHIRO, '''images[].url'''),
+            self._get_json_search_query(UrlType.CHIHIRO, '''promomedia[].materials[].urls[].url'''),
+            ]
 
-
-        ]
-
-        logger.info("activate()")
+        logger.debug("activate()")
 
     def deactivate(self):
         super().deactivate()
 
-        logger.info("deactivate()")
+        logger.debug("deactivate()")
 
     @event(PluginFunctions.get_urls)
     def my_get_urls(self, item_session: ItemSession):
@@ -70,7 +88,8 @@ class PsStoreJsonApiWpullPlugin(WpullPlugin):
         logger.info("get_urls() for url `%s`", the_url)
 
 
-        if the_url.startswith(VALKYRIE_URL_PREFIX) or the_url.startswith(CHIHIRO_URL_PREFIX):
+        if the_url.startswith(VALKYRIE_URL_PREFIX) \
+            or (the_url.startswith(CHIHIRO_URL_PREFIX) and not the_url.endswith(CHIHIRO_IMAGE_URL_SUFFIX)):
 
             the_type = None
             if the_url.startswith(VALKYRIE_URL_PREFIX):
@@ -87,9 +106,8 @@ class PsStoreJsonApiWpullPlugin(WpullPlugin):
                 item_session.add_child_url(iter_url)
         else:
 
-            logger.info("url doesn't start with JSON api prefix, not adding any new urls")
             # not one of the main JSON api urls, don't add any new urls
-
+            logger.info("url doesn't start with JSON api prefix, or had the /image suffix, not adding any new urls")
 
 
     def process_result(self, url_type:UrlType, item_session:ItemSession):
@@ -118,33 +136,44 @@ class PsStoreJsonApiWpullPlugin(WpullPlugin):
 
             logger.info("`process_result() - %s`: parsed successfully", url_type)
 
-            for iter_jmespath_expr in jmespath_expr_list:
+            for iter_json_search_query_obj in jmespath_expr_list:
 
-                result_set = None
-                result_obj = iter_jmespath_expr.search(json_obj)
+                try:
+                    iter_jmespath_expr = iter_json_search_query_obj.jmespath_compiled_query
 
-                logger.info("`process_result() - %s`: the expr `%s` found result: `%s`", url_type, iter_jmespath_expr, result_obj)
+                    result_set = None
+                    result_obj = iter_jmespath_expr.search(json_obj)
 
-                # convert into a set if its not
-                # it could be a bare string
-                if isinstance(result_obj, str):
-                    result_set = {result_obj}
-                else:
-                    # we got a list, convert to a set to deduplicate urls
-                    result_set = set(result_obj)
+                    logger.info("`process_result() - %s`: the expr `%s` found result: `%s`",
+                        url_type, iter_json_search_query_obj.jmespath_query, result_obj)
+
+                    # convert into a set if its not
+                    # it could be a bare string
+                    if isinstance(result_obj, str):
+                        result_set = {result_obj}
+                    else:
+                        # we got a list, convert to a set to deduplicate urls
+                        result_set = set(result_obj)
 
 
-                if result_set:
-                    urls_to_add_set.update(result_set)
+                    if result_set:
+                        urls_to_add_set.update(result_set)
+
+                except Exception as exc:
+
+                    logger.exception("Unhandled exception when processing the url `%s` with the JsonSearchQuery `%s`",
+                        url, iter_json_search_query_obj)
+
+                    raise exc
 
 
             logger.info("`process_result() - %s`: urls parsed from url `%s` were: `%s`", url_type, url, urls_to_add_set)
 
         except Exception as e:
 
-            logger.exception("`process_result() - %s`: Problem decoding the body as JSON for the url `%s`", url_type, url)
+            logger.exception("`process_result() - %s`: Problem decoding the body as JSON for the url `%s, returning empty set",
+                url_type, url)
             return set()
 
         return urls_to_add_set
-
 
